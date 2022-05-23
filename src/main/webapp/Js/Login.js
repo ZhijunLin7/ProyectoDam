d53eead64f2536217113316db2e2fdcbf0540c34
ZhijunLin7 <zlin1@cifpfbmoll.eu>
2022-05-23 12:38:28 +0200

    Pagina login hecho pero aun no conectado al base de datos

---
+++ src/main/webapp/Js/Login.js
@@ -0,0 +1,13 @@
+function AlLogin() {
+    document.getElementById("box1").style.display="none";
+    document.getElementById("box2").style.display="block";
+    document.getElementById("AlLogin").style.backgroundColor="peachpuff";
+    document.getElementById("AlRegistrar").style.backgroundColor="rgb(216,216,216)";
+}
+
+function AlRegistrar() {
+    document.getElementById("box1").style.display="block";
+    document.getElementById("box2").style.display="none";
+    document.getElementById("AlLogin").style.backgroundColor="rgb(216,216,216)";
+    document.getElementById("AlRegistrar").style.backgroundColor="peachpuff";
+}
